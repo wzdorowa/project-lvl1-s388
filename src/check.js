80a240ea82b05b806f390eecfc1e0cb519941e1b
Veronika Zdorova <w.zdorowa@gmail.com>
2018-12-06 19:13:56 +0300

    Исправлены замечания по шагу 4

--- src/check.js
+++ src/check.js
@@ -1,48 +1,28 @@
 import readlineSync from 'readline-sync';
 import greet from './greet';
 
-const yes = 'yes';
-const no = 'no';
-
 const getRandomInRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-
-const askQuestion = () => {
-  const randomNum = getRandomInRange(1, 10);
-  console.log(`Question: ${randomNum}`);
-  return randomNum;
-};
-
-const askAnswer = () => {
-  const request = readlineSync.question('Your answer: ');
-  return request;
-};
-const checkParity = (question) => {
-  if (question % 2 === 0) {
-    return yes;
-  }
-  return no;
-};
+const isEven = randomNum => ((randomNum % 2 === 0) ? 'yes' : 'no');
 
 const check = () => {
   const nameUser = greet();
-  let correctAnswers = 0;
-  for (let i = 0; i <= 3; i += 1) {
-    if (correctAnswers === 3) {
-      console.log(`Congratulation, ${nameUser}!`);
-      return;
-    }
-    const question = askQuestion();
-    const answer = askAnswer();
-    const parity = checkParity(question);
-    if (parity === answer) {
-      correctAnswers += 1;
+
+  for (let i = 0; i < 3; i += 1) {
+
+    const randomNum = getRandomInRange(1, 10);
+    const rightAnswer = isEven(randomNum);
+    console.log(`Question: ${randomNum}`);
+    const answer = readlineSync.question('Your answer: ');
+    
+    if (rightAnswer === answer) {
       console.log('Correct!');
     } else {
-      console.log(`${answer} is wrong answer ;(. Correct answer was ${parity}.`);
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${rightAnswer}.`);
       console.log(`Let's try again, ${nameUser}!`);
       return;
     }
   }
+  console.log(`Congratulation, ${nameUser}!`);
 };
 
 export default check;
